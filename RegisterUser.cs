b2102a1863fa768c4fa3efebad296f3fe93b955c
MadhuriKorke <madhurikorke1@gmail.com>
2025-09-11 22:13:58 +0530

    error

--- RegisterUser.cs
+++ RegisterUser.cs
@@ -28,7 +28,7 @@ namespace GAMEZONE
 
         private void cmbbxUserType_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            mmmmmmm
         }
 
         private void RegisterUser_Load(object sender, EventArgs e)
