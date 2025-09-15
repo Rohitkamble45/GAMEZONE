0be9d34d947c89c8aa5adcb3d15ca98b37327616
MadhuriKorke <madhurikorke1@gmail.com>
2025-09-15 18:56:56 +0530

    ss

--- RegisterUser.cs
+++ RegisterUser.cs
@@ -28,7 +28,7 @@ namespace GAMEZONE
 
         private void cmbbxUserType_SelectedIndexChanged(object sender, EventArgs e)
         {
-            poiufc;lkjfdfjklkjfdjkjfdd
+            poiufc;lkjfdfjklkjfdjkjfdd;
 
         }
 
